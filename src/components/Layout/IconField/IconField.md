904f8207df50e3d66943d0f1a0d0c63c3dddb7a9
Francisco Barceló <franciscobarcelo@MinideFrancisco.home>
2020-01-31 16:52:37 +0100

    -Remove Iconfield component

--- src/components/Layout/IconField/IconField.md
+++
@@ -1,7 +0,0 @@
-A very simple icon field select.
-
-```jsx
-import { IconField } from "architect-components-library";
-
-<IconField text="I MUST BE DREAMING frnk" />
-```
--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
 export { default as Button } from "components/Button";
 export { default as InputField } from "components/Layout/InputField/InputField";
-export { default as IconField } from "components/Layout/IconField/IconField";
